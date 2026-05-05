728d7bb4dcc432200937dabb6b9697f144f6e574
Andrew Cain <acain89@gmail.com>
2026-05-05 15:27:54 -0500

    PWA icons + manifest setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
+// app/layout.tsx
+
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import PwaServiceWorker from "./components/PwaServiceWorker";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -11,7 +14,6 @@ const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
-
 
 export const metadata: Metadata = {
   title: "RentFray",
@@ -28,6 +30,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <PwaServiceWorker />
         {children}
       </body>
     </html>
