0feb0835d298a2c3e53aed147ecd9ca07639ec3f
Andrew Cain <acain89@gmail.com>
2026-07-17 12:41:14 -0500

    Install Google Analytics GA4

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import PwaServiceWorker from "./components/PwaServiceWorker";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,6 +31,20 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+       <Script
+  src="https://www.googletagmanager.com/gtag/js?id=G-PZ5HUNRTS"
+  strategy="afterInteractive"
+/>
+
+<Script id="google-analytics" strategy="afterInteractive">
+  {`
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-PZ5HUNRTS');
+  `}
+</Script>
         <PwaServiceWorker />
         {children}
       </body>
