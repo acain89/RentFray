3e9e6d0f5abdb68f54f0271e985ba455ad5edc55
Andrew Cain <acain89@gmail.com>
2026-07-18 20:11:53 -0500

    Add Google Ads tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,8 @@
-// app/layout.tsx
-
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import PwaServiceWorker from "./components/PwaServiceWorker";
-import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,20 +29,22 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-       <Script
-  src="https://www.googletagmanager.com/gtag/js?id=G-PZ5HJJNRTS"
-  strategy="afterInteractive"
-/>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-PZ5HJJNRTS"
+          strategy="afterInteractive"
+        />
 
-<Script id="google-analytics" strategy="afterInteractive">
-  {`
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
+        <Script id="google-tags" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
 
-    gtag('config', 'G-PZ5HJJNRTS');
-  `}
-</Script>
+            gtag('config', 'G-PZ5HJJNRTS');
+            gtag('config', 'AW-18311599021');
+          `}
+        </Script>
+
         <PwaServiceWorker />
         {children}
       </body>
